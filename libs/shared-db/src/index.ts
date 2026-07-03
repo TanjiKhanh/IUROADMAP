f091f3f850e7033e8a37111cbf07551267f29a82
Khanh <ITCSIU23014@student.hcmiu.edu.vn>
2026-07-03 17:30:53 +0700

    test

--- libs/shared-db/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './prisma.module';
-export * from './prisma.service';
-// You can also export types if needed, though usually @prisma/client is global
-
-// 👇 CRITICAL: Re-export Prisma types so services can use 'User', 'Role', etc.
-export * from '@prisma/client';
